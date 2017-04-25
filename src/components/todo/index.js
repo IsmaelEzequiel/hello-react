f3b4b2df8fa4f9697d01f5d6fbec3c67c2aa886d
IsmaelEzequiel <kell.trollhammaren@hotmail.com>
2017-04-24 23:46:46 -0300

    using map to show values from a state

--- src/components/todo/index.js
+++ src/components/todo/index.js
@@ -3,15 +3,30 @@ import React, { Component } from 'react';
 import './index.css';
 
 class Todo extends Component {
+    constructor() {
+        super();
+        this.state = {
+            todos: [
+                {id: 1, name:'Todo one', isComplete: true},
+                {id: 2, name:'todo two', isComplete: false},
+                {id: 3, name:'todo three', isComplete: false},
+            ],
+            currentTodo: ''
+        }
+    }
+
     render() {
         return (
             <div className="Todo-app">
                 <form action="#" className="Form-input">
-                    <input type="text"/>
+                    <input type="text" placeholder="Hey, what do you wanna to add?" value={ this.state.currentTodo }/>
                 </form>
                 <ul className="List-todo">
-                    <li><input type="checkbox"/>Hello</li>
-                    <li><input type="checkbox"/>Heasdasdasllo</li>
+                    { this.state.todos.map(todo => 
+                        <li key={todo.id}>
+                            <input type="checkbox" defaultChecked={ todo.isComplete } />{ todo.name }
+                        </li>)
+                    }
                 </ul>      
             </div>
         );
